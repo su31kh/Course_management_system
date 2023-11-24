efe3b4234be55b8a5476fc7c45888c594686506f
unknown <adityasinghania3802@gmail.com>
2023-11-24 19:12:10 +0530

    Added Forgot Password Functionality

--- Code/Academix_Portal/urls.py
+++ Code/Academix_Portal/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from Academix_Portal import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
@@ -55,5 +56,21 @@ urlpatterns = [
     path('mycourse/<str:course_id>/studentlist/<str:id>/viewprofile',views.view_profile , name= "view_profile"),
     path('student_profile/change',views.update_profile, name="update_profile"),
     path('<str:course_id>/<str:student>',views.students_assignment , name= "students_assignment"),
-    path('logout',views.log_out,name='log_out')
+    path('logout/',views.log_out,name='log_out'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),
+         name="password_reset_complete"),
 ]
